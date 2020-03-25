887d29d13f60955e20012fc884308f63450ef7ad
Root <inventoxz@gmail.com>
2020-03-25 01:03:04 +0100

    _

--- __init__.py
+++ __init__.py
@@ -2,10 +2,45 @@
 Data sources:
 https://nssdc.gsfc.nasa.gov/planetary/factsheet/
 
+!add:
+use API to place planets according to specific epoch
+
+add:
+orbital eccentricity
+orbital inclination
+asteroid field generation
+
+buttons:
+
+	space: speeeeeed
+	wasd: control camera
+
+	up: enlarge sun
+	dn: shrink sun
+	lf: stop growth
+	rt: spawn plasma
+
+	f:  set sun to 1
+
 '''
 
-import pyglet, ratcave as rc, math
+import pyglet, ratcave as rc, math, os
 from pyglet.window import key
+
+# global factors
+speedFactor = 1
+distanceFactor = 5
+massFactor = .5
+
+# animation vars
+secs = 0  # seconds since program start
+class AnimStates:
+	state = None
+
+	GROWING = 1
+	SHRINKING = 2
+	PLASMA = 3
+
 
 # pyglet inits
 window = pyglet.window.Window(width=1280, height=960, screen=0)
@@ -14,17 +49,15 @@ window.push_handlers(keys)
 
 # ratcave inits
 default = rc.WavefrontReader(rc.resources.obj_primitives)
-# star = default.get_mesh("Sphere", position=(0, 0, 0))
-
-scene = rc.Scene(meshes=[], bgColor=(45 / 255, 45 / 255, 45 / 255))
-scene.light.position.y = 10_000
-
-scene.camera.rotation.x = -45
-scene.camera.position.xyz = 0, 7, 7
+scene = rc.Scene(
+	meshes=[],
+	bgColor=(45 / 255, 45 / 255, 45 / 255),
+	light=rc.Light(position=(0, 10_000, 0)),
+	camera=rc.Camera(position=(0, 7, 7), rotation=(-45, 0, 0)))
 
 
 class Planet:
-	def __init__(self, diameter, pos=(0, 0, 0), rotationSpeed=0, period=None, name="", children=None):
+	def __init__(self, diameter, pos=(0, 0, 0), day=24, period=None, name="", children=None, schedule=False):
 		"""
 		:param diameter:
 			radius of planet (0-1, not to scale)
@@ -32,46 +65,73 @@ class Planet:
 			distance from the sun in 3 axis (AU)
 		:param period:
 			orbital period (days to encircle the parent-celestial-object), 365.2 in earth's case
-		:param rotationSpeed:
-			the amount of earth days needed to rotate the planet 360 degrees
+		:param day:
+			hours in this object's day
 		"""
 
 		self.diameter = diameter
-		self.position = pos  # this is accurate in IRL AU units, however it is rendered at 3x the scale
+		self.position = pos  # this is accurate in IRL AU units (scaled down), however it is rendered at 3x the scale
 		self.period = period
-		self.rotationSpeed = rotationSpeed
+		self.day = day
 		self.name = name
 		self.children = children
 		# the default mesh diameter is 2, hence the halving in diameter below
+
 		self.mesh = default.get_mesh("Sphere",
-									position=[i * 3 for i in self.position],
+									position=[i * distanceFactor for i in self.position],
 									scale=0.5 * self.diameter)
+
 		scene.meshes.append(self.mesh)
-		pyglet.clock.schedule(self.update)
+		if schedule:
+			pyglet.clock.schedule(self.update)
 
 	def update(self, dt, planet=None):
-		self.mesh.rotation.y += self.rotationSpeed * dt
+		self.mesh.rotation.y += dt * speedFactor * (24 / self.day * 360)
 		if planet:
 			self.orbit(dt, planet)
 		if self.children:
 			for child in self.children:
 				child.update(dt, planet=self)
 
-	def orbit(self, dt, planet, factor=8):
+	def orbit(self, dt, planet, degrees=None):
 		x, y, z = self.position
 		ox, _, oz = planet.position
-
-		degrees = dt * factor / self.period * 360
+		# earth will take 365.2 secs to completely revolve at default speed
+		if not degrees:
+			degrees = dt * speedFactor / self.period * 360
 		radians = math.radians(degrees)
 
 		nx = (x - ox) * math.cos(radians) - ((z - oz) * math.sin(radians)) + ox
 		nz = (z - oz) * math.cos(radians) + ((x - ox) * math.sin(radians)) + oz
 
 		self.position = nx, y, nz
-		self.mesh.position.xyz = [i * 3 for i in self.position]
+		self.mesh.position.xyz = [i * distanceFactor for i in self.position]
 
 
 def update(dt):
+	global speedFactor, secs
+	secs += dt
+
+	# reset controls
+	if keys[key.F]:
+		AnimStates.state = None
+		sun.mesh.scale.xyz = 1, 1, 1
+
+	# state controls
+	AnimStates.state = \
+		AnimStates.GROWING if keys[key.UP] else \
+		AnimStates.SHRINKING if keys[key.DOWN] else \
+		AnimStates.PLASMA if keys[key.RIGHT] else \
+		None if keys[key.LEFT] else AnimStates.state
+
+	# perform actions based on current animation state
+	if AnimStates.state == AnimStates.GROWING:
+		sun.mesh.scale.xyz = [i + (dt/(massFactor*distanceFactor)) for i in sun.mesh.scale.xyz]
+
+	if AnimStates.state == AnimStates.SHRINKING:
+		sun.mesh.scale.xyz = [i - (dt/(massFactor*distanceFactor)) for i in sun.mesh.scale.xyz]
+
+	# camera control
 	if keys[key.D]:
 		scene.camera.position.x += dt * 5
 	if keys[key.A]:
@@ -81,34 +141,53 @@ def update(dt):
 	if keys[key.S]:
 		scene.camera.position.z += dt * 5
 
+	# speed control
+	if keys[key.SPACE]:
+		speedFactor = 32
+	else:
+		speedFactor = 1
+
 
 pyglet.clock.schedule(update)
+planetDiameter = lambda d: d/4879*.25*massFactor  # mercury dia/4 is fixed in relation to other planets
 
 sun = Planet(1,
 	name="Sun",
+	day=27,
+	schedule=True,
 	children=[
-		Planet(.25,
+		Planet(planetDiameter(4879),
 			name="Mercury",
 			pos=(.387, 0, 0),
+			day=4222.6,
 			period=88),
-		Planet(.75,
+		Planet(planetDiameter(12_104),
 			name="Venus",
 			pos=(.723, 0, 0),
+			day=2802,
 			period=224.7),
-		Planet(.75,
+		Planet(planetDiameter(12_756),
 			name="Earth",
 			pos=(1, 0, 0),
+			day=24,
 			period=365.2,
 			children=[
-				Planet(.1,
+				Planet(planetDiameter(3475),
 					name="Moon",
-					pos=(1.384, 0, 0),
-					period=27.3)
+					pos=(1.257, 0, 0), # 1.00257
+					day=708.7,
+					period=27.3),
 		]),
-		Planet(.5,
+		Planet(planetDiameter(6792),
 			name="Mars",
 			pos=(1.52, 0, 0),
-			period=687)
+			day=24.7,
+			period=687),
+		Planet(planetDiameter(142_984),
+			name="Jupiter",
+			pos=(5.2, 0, 0),
+			day=9.9,
+			period=4331)
 ])
 
 
